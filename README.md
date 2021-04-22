fd499f18108ee509ea54453f4624f67b07f096a5
Mario <mario@debvm>
2021-04-22 04:12:59 -0700

    build script for x264 and ffmpeg

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ZtPlayer
 Android video player based on ffmpeg
+
+## clone with recursive
+```
+git clone --recursive git@github.com/xxx.git
+```
+
+## build jni libraries
+### prerequisites
+Install pkg-config
+```
+sudo apt-get install pkg-config build-essentials
+```
+
+```
+cd app/src/main/jni
+./build_library.sh
+```
+
--- app/src/main/jni/build_library.sh
+++ app/src/main/jni/build_library.sh
@@ -1,22 +1,139 @@
 #!/bin/bash -ex
 
-# checkout libaries
-#git clone https://code.videolan.org/videolan/x264.git
-#git clone https://git.ffmpeg.org/ffmpeg.git
-#git clone https://github.com/opencv/opencv.git
-#git clone https://github.com/opencv/opencv_contrib.git
+echo "Configure the building toolchains properly before starting."
 
-NDK='/c/Users/800457/AppData/Local/Android/Sdk/ndk/22.1.7171670'
+NDK="$HOME/ndk"
+HOST='linux-x86_64'
+
 X264='x264'
-FFPMEG='ffmpeg'
+# x264 branch to checkout
+X264_BRANCH='stable'
+
+FFMPEG='ffmpeg'
+# ffmpeg branch to checkout
+FFMPEG_BRANCH='release/4.4'
+
 OPENCV='opencv'
 OPENCV_CONTRIB='opencv_contrib'
 
-echo $NDK
 
-cd $X264
-build_x264
 
-function build_x264 {
+TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST
+SYSROOT=$NDK/toolchains/llvm/prebuilt/$HOST/sysroot
+API=22
 
+TARGETPLATFORM='android'
+
+
+function setup_arch() {
+    arch=$1
+
+    case $arch in
+        
+        arm64)
+            # general
+            export PREFIX=./$TARGETPLATFORM/arm64
+            export TARGET=aarch64-linux-android
+
+            # for ffmpeg
+            export ARCH=aarch64
+            ;;
+
+        *)
+            echo "Unknown arch: $arch"
+            exit
+            ;;
+        
+    esac
+
+    # general
+    export AR=$TOOLCHAIN/bin/$TARGET-ar
+    export CC=$TOOLCHAIN/bin/$TARGET$API-clang
+    export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
+    # use gold version of ld
+    # export LD=$TOOLCHAIN/bin/$TARGET-ld.gold
+    export LD=$TOOLCHAIN/bin/ld
+    export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
+    export STRIP=$TOOLCHAIN/bin/$TARGET-strip
+
+    # ffmpeg
+    export NM=$TOOLCHAIN/bin/$TARGET-nm
 }
+
+
+function build_x264() {
+    arch=$1
+    setup_arch $arch
+
+  ./configure \
+      --prefix=$PREFIX \
+      --enable-static \
+      --enable-pic \
+      --host=aarch64-linux \
+      --cross-prefix=$TOOLCHAIN/bin/$TARGET- \
+      --sysroot=$SYSROOT
+
+  make
+  make install
+}
+
+
+function build_ffmpeg {
+    # https://github.com/bilibili/ijkplayer/issues/4093
+    # use --disable-linux-perf to fix B0 issues
+    arch=$1
+    setup_arch $arch
+
+    ./configure \
+        --target-os=android \
+        --prefix=$PREFIX \
+        --enable-static \
+        --disable-runtime-cpudetect \
+        --disable-doc \
+        --disable-debug \
+        --disable-ffmpeg \
+        --disable-ffprobe \
+        --disable-programs \
+        --disable-ffplay \
+        --enable-pic \
+        --target-os=linux \
+        --cross-prefix=$TOOLCHAIN/bin/$TARGET- \
+        --sysroot=$SYSROOT \
+        --cc=$CC \
+        --nm=$NM \
+        --ar=$AR \
+        --strip=$STRIP \
+        --ranlib=$RANLIB \
+        --pkg-config=`(which pkg-config)` \
+        --enable-small \
+        --enable-cross-compile \
+        --extra-libs="-lgcc" \
+        --arch=aarch64 \
+        --cross-prefix=$PREBUILT/bin/aarch64-linux-android- \
+        --extra-ldflags="-lx264 -nostdlib -lc -lm -ldl -llog" \
+        --enable-zlib \
+        --enable-gpl \
+        --enable-libx264 \
+        --disable-linux-perf \
+        --extra-cflags="-I../x264/android/arm64/include" \
+        --extra-ldflags="-L../x264/android/arm64/lib"
+
+    #make clean
+    make -j4
+    make install    
+}
+
+
+# cd $X264
+# git checkout $X264_BRANCH
+# build_x264 arm64
+# cd ..
+
+cd $FFMPEG
+git checkout $FFMPEG_BRANCH
+build_ffmpeg arm64
+cd ..
+
+# Local Variables:
+# indent-tabs-mode: nil
+# End:
